f98e31f7b27f0a5ced669db15a15a98aeeeb01fe
Gustavo Costa <gsantosaero@gmail.com>
2024-10-09 09:03:53 -0300

    exercicio16

---
+++ 11_trazendo_dados_fazendo_contas/exercicio16.py
@@ -0,0 +1,5 @@
+from math import trunc
+
+numero = float(input("Digite um valor: "))
+print(f"A parte inteira do número digitado é utilizando int: {int(numero)}")
+print(f"A parte inteira do número digitado utilizando trunc: {trunc(numero)}")
